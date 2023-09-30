cc7aba80cec9f1a5d2d5d0925f5910c893776595
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-09-30 19:59:08 +0200

    [REFACTOR] Function name change

--- backend/utils/generateProxyContract.ts
+++ backend/utils/generateProxyContract.ts
@@ -1,4 +1,4 @@
-export function generateProxyContract(signatures: string[]): string {
+export function generateReplicatedFunctionProxyContract(signatures: string[]): string {
     let proxyCode = `
 pragma solidity ^0.8.0;
 import "..."; // Include necessary Chainlink imports here for CCIP
@@ -50,6 +50,79 @@ contract CCIPProxy {
 `;
 
     proxyCode += `}`;
+
+    return proxyCode;
+}
+
+export function generateSingularFowardingProxyContract(): string {
+    // Contract template
+    let proxyCode = `
+pragma solidity ^0.8.0;
+import "..."; // Include necessary Chainlink imports here for CCIP
+
+contract CCIPProxy {
+    IRouterClient router;
+    LinkTokenInterface linkToken;
+    address public immutable targetAddress; 
+    address public owner;
+
+    mapping(string => bool) public allowList;
+
+    error NotEnoughBalance(uint256 currentBalance, uint256 calculatedFees);
+    error FunctionNotAllowlisted(string signature);
+    error NotOwner();
+
+    constructor(address _router, address _link, address _targetAddress) {
+        router = IRouterClient(_router);
+        linkToken = LinkTokenInterface(_link);
+        targetAddress = _targetAddress; // Initializing target address
+        owner = msg.sender; // Setting the deployer as the owner
+    }
+
+    modifier onlyOwner() {
+        if(msg.sender != owner) {
+            revert NotOwner();
+        }
+        _;
+    }
+
+    function addToAllowList(string memory signature) external onlyOwner {
+        allowList[signature] = true;
+    }
+
+    function removeFromAllowList(string memory signature) external onlyOwner {
+        allowList[signature] = false;
+    }
+
+    function forward(string memory signature, bytes memory data) external {
+        if(!allowList[signature]) {
+            revert FunctionNotAllowlisted(signature);
+        }
+        _forwardCCIPMessage(signature, data);
+    }
+
+    function _forwardCCIPMessage(string memory signature, bytes memory data) private {
+        // Constructing the CCIP message
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(address(this)), // This might need adjustment based on specific use-cases
+            data: abi.encodeWithSignature(signature, data),
+            tokenAmounts: new Client.EVMTokenAmount[](0),
+            extraArgs: Client._argsToBytes(
+                Client.EVMExtraArgsV1({gasLimit: 200_000, strict: false})
+            ),
+            feeToken: address(linkToken)
+        });
+
+        uint256 fees = router.getFee(_destinationChainSelector, message);
+
+        if (fees > linkToken.balanceOf(address(this)))
+            revert NotEnoughBalance(linkToken.balanceOf(address(this)), fees);
+
+        linkToken.approve(address(router), fees);
+
+        router.ccipSend(_destinationChainSelector, message); 
+    }
+}`;
 
     return proxyCode;
 }
