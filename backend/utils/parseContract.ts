73bf693d68a2c07f50c9eaea1aa100531fb871e1
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-09-30 17:09:13 +0200

    [FEAT] Parse smart contract

---
+++ backend/utils/parseContract.ts
@@ -0,0 +1,60 @@
+import solc from 'solc';
+
+// This function compiles the Solidity contract and returns the JSON output.
+function compileContract(source: string): any {
+    const input = {
+        language: 'Solidity',
+        sources: {
+            'contract.sol': {
+                content: source
+            }
+        },
+        settings: {
+            outputSelection: {
+                '*': {
+                    '*': ['*']
+                }
+            }
+        }
+    };
+
+    const output = JSON.parse(solc.compile(JSON.stringify(input)));
+    return output.contracts['contract.sol'];
+}
+
+// This function extracts public and external functions from a contract's ABI.
+function extractFunctions(contractABI: any[]): string[] {
+    return contractABI
+        .filter(item => (item.type === 'function' && (item.stateMutability === 'nonpayable' || item.stateMutability === 'payable')))
+        .map(func => func.name);
+}
+
+// This function generates function signatures from the function names and their inputs.
+function generateSignatures(contractABI: any[]): string[] {
+    return contractABI
+        .filter(item => (item.type === 'function' && (item.stateMutability === 'nonpayable' || item.stateMutability === 'payable')))
+        .map(func => {
+            const params = func.inputs.map((input: any) => input.type).join(',');
+            return `${func.name}(${params})`;
+        });
+}
+
+// Example Usage
+const source = `
+pragma solidity ^0.8.0;
+
+contract Example {
+    function mint(address receiver, uint256 amount) public {}
+    function burn(address holder) external {}
+}
+`;
+
+const compiled = compileContract(source);
+const contractName = Object.keys(compiled)[0];
+const contractABI = compiled[contractName].abi;
+
+const functions = extractFunctions(contractABI);
+console.log('Functions:', functions);
+
+const signatures = generateSignatures(contractABI);
+console.log('Signatures:', signatures);
