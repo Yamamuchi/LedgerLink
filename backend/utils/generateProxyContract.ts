44b24824b631d8ebeb5d82ee88264f1e84fdd78e
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-09-30 17:31:09 +0200

    [FEAT] Generate CCIP proxy interact contract

---
+++ backend/utils/generateProxyContract.ts
@@ -0,0 +1,20 @@
+function generateProxyContract(contractName: string, contractABI: any[], signatures: string[]): string {
+    let proxyCode = `
+pragma solidity ^0.8.0;
+import "..."; // Your imports here, e.g. for Chainlink's CCIP
+
+contract CCIPProxy {
+`;
+
+    signatures.forEach(sig => {
+        proxyCode += `
+    function ${sig} {
+        // Chainlink CCIP call to the primary contract here
+    }
+`;
+    });
+
+    proxyCode += `}`;
+
+    return proxyCode;
+}
