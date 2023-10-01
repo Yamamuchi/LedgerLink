8823ee7681ec6c2a8e201ed994d4e07f14df3eee
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-10-01 05:56:28 +0200

    [FIX] endpoint

--- backend/src/api/routes/deployContractsRoute.ts
+++ backend/src/api/routes/deployContractsRoute.ts
@@ -3,6 +3,6 @@ import { deploySmartContracts } from '../controllers/deployContractsController';
 
 const deployContractsRoute = Router();
 
-deployContractsRoute.post('/deploy', deploySmartContracts);  // Use POST to receive the contract source code
+deployContractsRoute.post('/', deploySmartContracts); // Use POST to receive the contract source code
 
 export default deployContractsRoute;
