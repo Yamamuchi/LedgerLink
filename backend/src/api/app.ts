ef3596e85464ee78a592ca39cb023aa90fed82fd
Harvey Yorke <108807429+Yamamuchi@users.noreply.github.com>
2023-10-01 05:56:18 +0200

    [FEAT] cors

--- backend/src/api/app.ts
+++ backend/src/api/app.ts
@@ -2,8 +2,10 @@ import express from 'express';
 import deployContractsRoute from './routes/deployContractsRoute';
 
 const app = express();
+const cors = require('cors');
 
 app.use(express.json()); // for parsing application/json
+app.use(cors());
 
 // Define the route
 app.use('/deploy', deployContractsRoute);
